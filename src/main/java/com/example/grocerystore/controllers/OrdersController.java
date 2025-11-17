73174cb26be554bb69b514aa532b69f01bebcd41
Mikhail Churakov <mikhailchurakov@Mikhails-MacBook-Pro.local>
2025-11-17 12:15:38 +0100

    fixed bug with merge of identical items

--- src/main/java/com/example/grocerystore/controllers/OrdersController.java
+++ src/main/java/com/example/grocerystore/controllers/OrdersController.java
@@ -3,6 +3,7 @@ package com.example.grocerystore.controllers;
 import com.example.grocery.api.orders.OrdersApi;
 import com.example.grocery.model.orders.CreateOrderRequest;
 import com.example.grocery.model.orders.CreateOrderResponse;
+import com.example.grocerystore.services.orders.OrdersBeautifierService;
 import com.example.grocerystore.services.orders.OrdersService;
 import com.example.grocerystore.services.orders.OrdersValidationService;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +17,12 @@ public class OrdersController implements OrdersApi {
 
     private final OrdersService ordersService;
     private final OrdersValidationService validationService;
+    private final OrdersBeautifierService beautifierService;
 
     @Override
     public ResponseEntity<CreateOrderResponse> ordersPost(CreateOrderRequest createOrderRequest) {
         validationService.validateItems(createOrderRequest);
+        beautifierService.mergeItems(createOrderRequest);
         CreateOrderResponse response = ordersService.evaluateOrder(createOrderRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
